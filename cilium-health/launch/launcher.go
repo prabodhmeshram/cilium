bc376ef58ea47b5805f8723b0ad7595639aeab43
André Martins <andre@cilium.io>
2019-06-13 17:29:57 +0200

    daemon: do not get all nodes in "cluster" probe
    As "cluster" probe was being used to copy all nodes discovered by the
    nodemanager it causes a lot of unnecessary memory allocations for a
    large cluster. For this reason the "cluster" probe will stop being used
    to fetch the node information from the nodemanager and instead clients
    that want to fetch the node list should use "GetClusterNodes" method
    instead.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- cilium-health/launch/launcher.go
+++ cilium-health/launch/launcher.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2017-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -52,7 +52,7 @@ func (ch *CiliumHealth) Run() {
 	for {
 		cli, err := ciliumPkg.NewDefaultClient()
 		if err == nil {
-			if _, err = cli.Daemon.GetHealthz(nil); err == nil {
+			if _, err = cli.Daemon.GetClusterNodes(nil); err == nil {
 				break
 			}
 		}
